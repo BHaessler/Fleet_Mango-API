474d9ed8ddc4845bb8ccee6b7900eb88f595b8d4
BHaessler <e.a.haessler@gmail.com>
2024-11-14 16:00:52 -0500

    making license plate unique

--- shop_manager/cars/models.py
+++ shop_manager/cars/models.py
@@ -1,12 +1,13 @@
-# cars/models.py
+# models.py
+
 from django.db import models
 
 class Car(models.Model):
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
-    year = models.PositiveIntegerField()
+    year = models.IntegerField()
     color = models.CharField(max_length=50)
-    license_plate = models.CharField(max_length=20, unique=True)
+    license_plate = models.CharField(max_length=20, unique=True)  # Ensure license_plate is unique
 
     def __str__(self):
-        return f"{self.make} {self.model} ({self.year})"
+        return f'{self.make} {self.model} ({self.year}) - {self.license_plate}'
--- shop_manager/cars/serializers.py
+++ shop_manager/cars/serializers.py
@@ -1,8 +1,17 @@
-# cars/serializers.py
+# serializers.py
+
 from rest_framework import serializers
 from .models import Car
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
-        fields = '__all__'  # This will include all fields from the Car model
+        fields = '__all__'
+
+    def validate_license_plate(self, value):
+        """
+        Check if the license plate is unique.
+        """
+        if Car.objects.filter(license_plate=value).exists():
+            raise serializers.ValidationError("This license plate is already taken.")
+        return value
